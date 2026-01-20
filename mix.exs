e193b23b8a3a0c28d3d820d5a25fedbc9fa84dd2
YaanitGran <yaanit.granados@bunsan.io>
2026-01-19 23:54:00 -0600

    Se quitala configuracion del ejecutable escript.build

--- mix.exs
+++ mix.exs
@@ -7,8 +7,7 @@ defmodule ProcesadorArchivos.MixProject do
       version: "0.1.0",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
-      deps: deps(),
-      escript: [main_module: ProcesadorArchivos.CLI]
+      deps: deps()
     ]
   end
 
